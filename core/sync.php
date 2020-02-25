384778f82c4049326b6f5881b634e65122bafc47
crystaldaking <crystaldaking@outlook.com>
2020-02-25 14:26:29 +0300

    Import all another elements

--- core/sync.php
+++ core/sync.php
@@ -17,6 +17,13 @@ if ($argc < 1){
 if ($argv[1] == 'import') {
     /** @var modTemplate[] $templates */
     $templates = $modx->getCollection(modTemplate::class);
+    /** @var modChunk[] $chunks */
+    $chunks = $modx->getCollection(modChunk::class);
+    /** @var modSnippet[] $snippets */
+    $snippets = $modx->getCollection(modSnippet::class);
+    /** @var modPlugin[] $plugins */
+    $plugins = $modx->getCollection(modPlugin::class);
+
     foreach ($templates as $template) {
         /** @var modCategory $category */
         $category = $template->getOne('Category');
@@ -34,6 +41,66 @@ if ($argv[1] == 'import') {
             $template->set('static_file', $savePath . 'templates/' . $name);
             if ($template->save()) {
                 $modx->log(MODX_LOG_LEVEL_INFO, "Template was imported: \"{$name}\"");
+            }
+        }
+    }
+    foreach ($chunks as $chunk){
+        /** @var modCategory $category */
+        $category = $chunk->getOne('Category');
+        $source = $chunk->toArray();
+        $source['category'] = $category ? $category->category : null;
+        if (!empty($source['content'])){
+            $content = $source['content'];
+            $name = trim($source['name']) . '.tpl';
+            if (file_put_contents($savePath . 'chunks/' . $name, $content) === false) {
+                $modx->log(MODX_LOG_LEVEL_ERROR, "Error while saving chunk into file: \"{$name}\"");
+                continue;
+            }
+            $chunk->set('static',1);
+            $chunk->set('source',1);
+            $chunk->set('static_file',$savePath.'chunks/'.$name);
+            if ($chunk->save()){
+                $modx->log(MODX_LOG_LEVEL_INFO, "Chunk was imported: \"{$name}\"");
+            }
+        }
+    }
+    foreach ($snippets as $snippet){
+        /** @var modCategory $category */
+        $category = $snippet->getOne('Category');
+        $source = $snippet->toArray();
+        $source['category'] = $category ? $category->category : null;
+        if (!empty($source['content'])){
+            $content = $source['content'];
+            $name = trim($source['name']) . '.php';
+            if (file_put_contents($savePath . 'snippets/' . $name, $content) === false) {
+                $modx->log(MODX_LOG_LEVEL_ERROR, "Error while saving snippet into file: \"{$name}\"");
+                continue;
+            }
+            $snippet->set('static',1);
+            $snippet->set('source',1);
+            $snippet->set('static_file',$savePath.'snippets/'.$name);
+            if ($snippet->save()){
+                $modx->log(MODX_LOG_LEVEL_INFO, "Snippet was imported: \"{$name}\"");
+            }
+        }
+    }
+    foreach ($plugins as $plugin) {
+        /** @var modCategory $category */
+        $category = $plugin->getOne('Category');
+        $source = $plugin->toArray();
+        $source['category'] = $category ? $category->category : null;
+        if (!empty($source['content'])){
+            $content = $source['plugincode'];
+            $name = trim($source['name']) . '.php';
+            if (file_put_contents($savePath . 'plugins/' . $name, $content) === false) {
+                $modx->log(MODX_LOG_LEVEL_ERROR, "Error while saving plugin into file: \"{$name}\"");
+                continue;
+            }
+            $plugin->set('static',1);
+            $plugin->set('source',1);
+            $plugin->set('static_file',$savePath.'plugins/'.$name);
+            if ($plugin->save()){
+                $modx->log(MODX_LOG_LEVEL_INFO, "Plugin was imported: \"{$name}\"");
             }
         }
     }
